d0bbc9a691fdac19e478a335bd2337334c017003
wjpe <86629087+wjpe@users.noreply.github.com>
2021-10-02 06:40:40 -0500

    Octubre 2

---
+++ Semana3/ProyectoVjs/calculadora/src/components/CalculadoraBasica.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <titulo />
+    <h1>Calculadora Básica</h1>
+
+    <hr />
+    <input type="number" v-model="n1"/>
+    <br />
+    <input type="number" v-model="n2"/>
+    <br >
+    <button @click="suma()">Sumar</button>
+    <button @click="resta()">Restar</button>
+    <br />
+    {{resp}}
+
+  </div>
+</template>
+
+<script>
+import Titulo from './Titulo.vue';
+export default {
+    name: "CalculadoraBasica",
+    components: { Titulo },
+    data: () =>{
+        return{
+            n1: null,
+            n2: null,
+            resp: "",
+        };
+    },
+    methods: {
+        suma(){
+            this.resp = parseFloat(this.n1) + parseFloat(this.n2) 
+        },
+        resta(){
+            this.resp = parseFloat(this.n1) - parseFloat(this.n2) 
+        }
+    },
+
+};
+
+</script>
+
+<style>
+
+</style>
---
+++ Semana3/ProyectoVjs/calculadora/src/components/Titulo.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+      <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat sit mollitia eligendi, 
+          maiores ducimus labore tempore voluptatem rem hic fuga minima, aliquam, atque quis enim officiis ratione. 
+          Repellendus, dolor. Dolorem.
+      </p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
