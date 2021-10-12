48796dce27f585bfc4b35cd542dfb5f58e54d499
wjpe <86629087+wjpe@users.noreply.github.com>
2021-10-11 19:23:56 -0500

    Calculadora basica instalado vuetify y router corregido las rutas, con conexion api futurama

---
+++ Semana3/ProyectoVjs/calculadora/src/components/Personajes.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h2>Personajes</h2>
+    <ul>
+      <li v-for="item in personajes" :key="item.Name">
+        {{item.Name}} - {{item.Planet}} - {{item.Status}}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import store from "../store/index.js";
+
+export default {
+  data: () => {
+    return {};
+  },
+  methods: {},
+  created: () => {
+    //Acceder a las actions del Store
+    store.dispatch("getPersonajes");
+  },
+  computed: {
+    personajes: () => {
+      return store.state.personajes;
+    }
+  },
+};
+</script>
+
+<style></style>
---
+++ Semana3/ProyectoVjs/calculadora/src/views/Personajes.vue
@@ -0,0 +1,19 @@
+<template>
+  <personajes />
+</template>
+
+<script>
+import Personajes from '../components/Personajes.vue'
+
+export default {
+    components: {
+        Personajes
+    },
+
+}
+</script>
+
+<style>
+
+</style>
+Personajes
